131364cb76238ff341a874f6e1ecdbbb4104237a
문상인 <moon@munsang-in-ui-MacBookPro.local>
2021-02-16 15:37:34 +0900

    js ver1

--- app.js
+++ app.js
@@ -1,4 +1,12 @@
-const canvas = document.getElementById("jsCanvas")
+const canvas = document.getElementById("jsCanvas");
+const ctx = canvas.getContext("2d");
+
+// Give pixel modifier the pixel size 
+canvas.width = 500;
+canvas.height = 500;
+
+ctx.strokeStyle = "#2c2c2c";
+ctx.lineWidth = 2.5;
 
 let painting = false;
 
@@ -6,11 +14,22 @@ function stopPainting() {
     painting = false;
 }
 
+function startPainting() {
+    painting = true;
+}
+
 function onMouseMove(event) {
     //console.log(event);
     const x = event.offsetX;
     const y = event.offsetY;
-    console.log(x, y);
+    //console.log(x, y);
+    if(!painting) {
+        ctx.beginPath();
+        ctx.moveTo(x, y);
+    } else {
+        ctx.lineTo(x, y);
+        ctx.stroke();
+    }
 }
 
 function onMouseDown(event) {
@@ -18,14 +37,9 @@ function onMouseDown(event) {
     painting = true;
 }
 
-function onMouseUp(event) {
-    stopPainting();
-
-}}
-
 if(canvas) {
     canvas.addEventListener("mousemove", onMouseMove);
-    canvas.addEventListener("mousedown", onMouseDown);
-    canvas.addEventListener("mouseup", onMouseUp);
-    canvas.addEventListener("mouseleave" stopPainting);
+    canvas.addEventListener("mousedown", startPainting);
+    canvas.addEventListener("mouseup", stopPainting);
+    canvas.addEventListener("mouseleave", stopPainting);
 }
